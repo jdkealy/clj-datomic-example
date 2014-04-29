490c40ce9af27faa9698eaa12b444260caaebedf
John Kealy <jdkealy@gmail.com>
2014-04-28 21:37:47 -0400

    initial commit

---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject clj-cms "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :ring {:handler clj-cms.core/app
+         :auto-reload? true
+
+         }
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-ring "0.8.10"]]
+  :dependencies [
+
+                 [org.clojure/clojure "1.5.1"]
+                 [ring/ring "1.2.2"]
+                 [com.cemerick/friend "0.2.0"]
+                 [com.datomic/datomic-free "0.8.4020.26"]
+                 [cheshire "4.0.3"]
+                 [enlive "1.1.5"]
+                 [bultitude "0.1.7"]
+                 [hiccup "1.0.1"]
+                 [fogus/ring-edn "0.2.0"]
+                 [ring.middleware.logger "0.4.0"]
+                 [compojure "1.1.6"]])
---
+++ src/clj_cms/core.clj
@@ -0,0 +1,92 @@
+(ns clj-cms.core
+  (:use
+   [ring.adapter.jetty             :only [run-jetty]]
+   [compojure.core                 :only [defroutes GET POST DELETE PUT ANY]]
+   [ring.middleware.params         :only [wrap-params]]
+                                        ;[ring.middleware.logger :as log]
+   [ring.middleware  keyword-params file file-info stacktrace reload]
+   )
+  (:require
+   [datomic.api :as d]
+   [compojure.route :as route]
+   [cheshire.core :as cc]
+   [cemerick.friend :as friend]
+   (cemerick.friend [workflows :as workflows]
+                    [credentials :as creds])
+   [compojure.handler :as handler]
+   ))
+
+(def uri "datomic:free://localhost:4334/blog")
+
+;; create database
+(d/create-database uri)
+
+;; connect to database
+(def conn (d/connect uri))
+
+;; parse schema edn file
+(def schema-tx (read-string (slurp "resources/schema/users.edn")))
+
+;; submit schema transaction
+(d/transact conn schema-tx)
+
+(defn get-user [id]
+  (let [todo
+        (d/touch (d/entity (d/db conn) id))
+        ]
+    {:username (:user/username todo) :password (:user/password todo) :id (:db/id todo)}))
+
+(defn create-user [username password]
+  (let [
+        todo @(d/transact
+               conn
+               [{:user/username username :user/password (creds/hash-bcrypt password) :db/id #db/id[:db.part/user]}])
+        ]
+    (get-user (first (vals (:tempids todo))))))
+
+(defn load-user-record [uname]
+  (let [
+        db (d/db conn)
+        users (d/q
+               '[:find ?e
+                 :in $ ?uname
+                 :where
+                 [?e :user/username ?uname]
+                 ]
+               db uname)]
+    (if-let [user (first (first users))]
+      (get-user (first (first users)))
+      false)))
+
+(defn index-page []
+  {
+   :status 200
+   :headers {"Content-Type" "text/html; charset=utf-8"}
+   :body (slurp "resources/layout.html")})
+
+(defroutes routes
+  (GET "/" [] (index-page))
+  (GET "/authorized" request
+       (friend/authorize #{::user} "This page can only be seen by authenticated users."))
+  (GET "/admin" request
+       (friend/authorize #{::admin} "This page can only be seen by authenticated users."))
+  (GET "/login" [] (index-page))
+  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
+  (route/resources "/")
+  (route/not-found "<h1>Page not found</h1>"))
+
+(def app-routes (
+          ->
+          routes
+          wrap-params
+          wrap-keyword-params
+          wrap-reload
+         ))
+
+(def app
+  (handler/site
+   (friend/authenticate app-routes
+                        {
+                         :login-uri "/login"
+                         :credential-fn (partial creds/bcrypt-credential-fn load-user-record)
+                         :workflows [(workflows/interactive-form)]})))
