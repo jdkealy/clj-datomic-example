d2941a7200029968c101ff96db15160350e23990
John Kealy <jdkealy@gmail.com>
2014-05-03 00:21:49 -0400

    cljs deps

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
          :auto-reload? true}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-ring "0.8.10"]]
-  :source-paths ["src/clj"]
+  :plugins [[lein-ring "0.8.10"]
+            [lein-cljsbuild "1.0.2"]]
+  :source-paths ["src/clj" "src/cljs"]
   :dependencies [
 
                  [org.clojure/clojure "1.5.1"]
@@ -20,4 +21,16 @@
                  [fogus/ring-edn "0.2.0"]
                  [ring.middleware.logger "0.4.0"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [compojure "1.1.6"]])
+                 [compojure "1.1.6"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [om "0.5.3"]
+                 [cljs-ajax "0.2.3"]
+                 [secretary "1.1.0"]]
+  :cljsbuild {:builds [{
+                        :source-paths ["src/cljs"]
+                        :compiler {
+                                   :output-dir "resources/public/js/out"
+                                   :output-to "resources/public/js/main.js"
+                                   :optimizations :none
+                                   :source-map true
+                                        }}]})
